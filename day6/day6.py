9f6f716c5b42a8d6055d7edf4f9e0f94a1b9bf02
Patrice Sorrant <48038618+psopsopso@users.noreply.github.com>
2022-12-12 23:55:56 -0500

    Complete part two

--- day6/day6.py
+++ day6/day6.py
@@ -3,9 +3,22 @@ with open("inputDay6.txt") as f:
 
 # Part 1
 
-for i in range(len(data)):
-    checkMarkerList = data[i:i+4]
-    checkMarkerSet = set(checkMarkerList)
-    if len(checkMarkerList) == len(checkMarkerSet):
-        print(i+4)
-        break
+
+def findMarkerPartOne(data):
+    for i in range(len(data)):
+        checkMarkerList = data[i:i+4]
+        checkMarkerSet = set(checkMarkerList)
+        if len(checkMarkerList) == len(checkMarkerSet):
+            print(i+4)
+            break
+
+# Part 2
+
+
+def findMarkerPartTwo(data):
+    for i in range(len(data)):
+        checkMarkerList = data[i:i+14]
+        checkMarkerSet = set(checkMarkerList)
+        if len(checkMarkerList) == len(checkMarkerSet):
+            print(i+14)
+            break
